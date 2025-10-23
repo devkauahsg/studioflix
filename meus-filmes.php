a4875bb7d6455037b27374a38d8bbf6479de369e
KaioV05 <beatriz.vitoria.costa2018@gmail.com>
2025-10-23 20:15:13 -0300

    Lista de Meus Filmes no StudioFlix

---
+++ meus-filmes.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="styles/filmes.css">
+
+    <title>Meus filmes - Studioflix</title>
+</head>
+<body>
+    <main>
+        <header>
+            <h1>STUDIOFLIX</h1>
+            <i class="fa-solid fa-bars"></i>
+        </header>
+
+        <h1>Meus Filmes</h1>
+        
+        <div class="filmes-row">
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            <div class="filme">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores">
+                <h2>Vingadores Ultimato (2019)</h2>
+                <button>ADICIONAR NA LISTA</button>
+            </div>
+            
+        </div>
+    
+    </main>
+
+    <footer>
+        <div class="container-footer">
+            <div class="footer-content">
+                <div class="footer-info">
+                    <h1>Sua rede social cinéfila!</h1>
+                    <p>Pronto para mergulhar na melhor comunidade de filmes? Comece agora mesmo.</p>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2025 Abner Ferreira, Kauã Henrique, Pedro Paulo e Raquel Menezes.</p>
+                <p>Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
