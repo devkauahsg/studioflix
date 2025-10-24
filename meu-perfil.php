23577cfdba49282eede7d950fd953e3800c605d6
KaioV05 <beatriz.vitoria.costa2018@gmail.com>
2025-10-23 21:18:16 -0300

    Perfil do Usuário Studioflix

---
+++ meu-perfil.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="styles/meu-perfil.css">
+
+    <title>Meu perfil - Studioflix</title>
+</head>
+<body>
+    <header>
+        <h1>STUDIOFLIX</h1>
+        <i class="fa-solid fa-bars"></i>
+    </header>
+
+    <main>
+        <div class="usuario">
+            <img src="assets/homem-perfil.jpg" alt="foto-perfil">
+            <h1>Kaua_hsg</h1>
+        </div>
+
+        <div class="meu-feed">
+            <h2>Meu Feed</h2>
+            <div class="filmes">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+                <img src="assets/poster-vingadores.jpg" alt="Vingadores Ultimato 2019">
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container-footer">
+            <div class="footer-content">
+                <div class="footer-info">
+                    <h1>Sua rede social cinéfila!</h1>
+                    <p>Pronto para mergulhar na melhor comunidade de filmes? Comece agora mesmo.</p>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2025 Abner Ferreira, Kauã Henrique, Pedro Paulo e Raquel Menezes.</p>
+                <p>Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
