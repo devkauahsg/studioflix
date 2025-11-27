a863dc68787fdb72a019c805d26434ce77d7a8fe
Thammy Silva <thamirysmatos@yahoo.com.br>
2025-11-27 19:45:56 -0300

    Código Tela Inicial

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="styles/studioflix.css">
+
+    <link rel="shortcut icon" href="assets/studioflix.ico" type="image/x-icon">
+
+    <title>StudioFlix - A rede social dos cinéfilos</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <div class="studioflix">
+                <img src="assets/logo-studioflix.png" alt="Logo StudioFlix">
+                <h1>Studioflix</h1>
+            </div>
+
+            <button class="hamburguer"></button>
+
+            <ul class="nav-list">
+                <li class="link-hover"><a href="login.php" class="login">Faça login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div class="central">
+            <h1>STUDIOFLIX</h1>
+            <h2>CONHEÇA O SEU CLUBE DE FILMES!</h2>
+            <p>Comente sobre os seus filmes favoritos.</p>
+
+            <a class="saiba-mais" href="#saiba-mais">SAIBA MAIS</a>
+        </div>
+    </main>
+    
+    <section id="saiba-mais">
+        <h1>PORQUE USAR O STUDIOFLIX?</h1>
+
+        <div class="cards">
+            <div class="cards-row">
+                <div class="card">
+                    <h1>⭐</h1>
+                    <h2>Dê sua opinião</h2>
+                    <p>Avalie cada filme com comentários.</p>
+                    <p><strong>Sua opinião pode ajudar outras pessoas a descobrirem se vai valer (ou não) aquela pipoquinha!</strong></p>
+                </div>
+                <div class="card">
+                    <h1>👀</h1>
+                    <h2>Descubra novos filmes</h2>
+                    <p>Explore o que outros usuários estão assistindo e encontre recomendações.</p>
+                    <p><strong>Nunca mais ficará naquela dúvida de qual filme assistir hoje!</strong></p>
+                </div>
+            </div>
+            <div class="cards-row">
+                <div class="card">
+                    <h1>❤️</h1>
+                    <h2>Comunidade cinéfila</h2>
+                    <p>Você entrará em uma rede feita para quem ama filmes como você.</p>
+                    <p><strong>A sua opinião também faz o cinema acontecer!</strong></p>
+                </div>
+                <div class="card">
+                    <h1>💡</h1>
+                    <h2>Interface simples e amigável</h2>
+                    <p>Ambiente feito especialmente para os amantes de cinema.</p>
+                    <p><strong>Fácil e simples para navegar e interagir!</strong></p>
+                </div>
+            </div>
+        </div>
+
+        <a class="comece-agora" href="login.php">COMECE AGORA</a>
+
+        <h1 class='title-oscar'>Seção Óscar</h1>
+
+        <div class="cards">
+            <div class="cards-row">
+                <div class="card-oscar">
+                    <img src="assets/joaquim-phoenix.jpg" alt="Joaquim Phoenix">
+                    <h2>🏆 Joaquin Phoenix</h2>
+                    <p>Ganhador do oscar por sua atuação no filme Coringa (2020)</p>
+                </div>
+                <div class="card-oscar">
+                    <img src="assets/anthony.jpg" alt="Anthony Hopkins">
+                    <h2>🏆 Anthony Hopkins</h2>
+                    <p>Ganhador do oscar por sua atuação no filme O Pai (2021)</p>
+                </div>
+            </div>
+            <div class="cards-row">
+                <div class="card-oscar">
+                    <img src="assets/will-smith.jpg" alt="Will Smith">
+                    <h2>🏆 Will Smith</h2>
+                    <p>Ganhador do oscar por sua atuação no filme Rei Ricardo (2022)</p>
+                </div>
+                <div class="card-oscar">
+                    <img src="assets/brendan.jpg" alt="Brendan Fraser">
+                    <h2>🏆 Brendan Fraser</h2>
+                    <p>Ganhador do oscar por sua atuação no filme A Baleia (2023)</p>
+                </div>
+            </div>
+            <div class="cards-row">
+                <div class="card-oscar">
+                    <img src="assets/ciliann.jpg" alt="Cilian Murphy">
+                    <h2>🏆 Cilian Murphy</h2>
+                    <p>Ganhador do oscar por sua atuação no filme oppenheimer (2023)</p>
+                </div>
+                <div class="card-oscar">
+                    <img src="assets/anoraa.jpg" alt="Anora">
+                    <h2>🏆 Anora</h2>
+                    <p>Filme ganhador do Oscar de 2024</p>
+                </div>
+            </div>
+            <div class="cards-row">
+                <div class="card-oscar">
+                    <img src="assets/oppenheimerr.jpg" alt="Oppenheimer">
+                    <h2>🏆 Oppenheimer</h2>
+                    <p>Filme ganhador do Oscar de 2023</p>
+                </div>
+                <div class="card-oscar">
+                    <img src="assets/tudoemtodotempo.jpg" alt="Tudo em Todo o Lugar ao Mesmo Tempo">
+                    <h2>🏆 Tudo em Todo o Lugar ao Mesmo Tempo</h2>
+                    <p>Filme ganhador do Oscar de 2022</p>
+                </div>
+            </div>
+            <div class="cards-row">
+                <div class="card-oscar">
+                    <img src="assets/ritmo-do-coracaoo.jpg" alt="No Ritmo do Coração">
+                    <h2>🏆 No Ritmo do Coração</h2>
+                    <p>Filme ganhador do Oscar de 2021</p>
+                </div>
+                <div class="card-oscar">
+                    <img src="assets/nomadlandd.jpg" alt="Filme ganhador do Oscar de 2020">
+                    <h2>🏆 Nomadland</h2>
+                    <p>Filme ganhador do Oscar de 2020</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <footer>
+        <div class="container-footer">
+            <div class="footer-content">
+                <div class="footer-info">
+                    <h1>Sua rede social cinéfila!</h1>
+                    <p>Pronto para mergulhar na melhor comunidade de filmes? Comece agora mesmo.</p>
+                    <a class="comece-agora" href="login.php">COMECE AGORA</a>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2025 Abner Ferreira, Kauã Henrique, Pedro Paulo e Raquel Menezes.</p>
+                <p>Todos os direitos reservados.</p>
+            </div>
+        </div>
+    </footer>
+
+    <?php 
+        if ($_GET['msg']) {
+            print_r("<script>alert('{$_GET['msg']}')</script>");
+        }
+    ?>
+    <script src="scripts/header.js"></script>
+</body>
+</html>
