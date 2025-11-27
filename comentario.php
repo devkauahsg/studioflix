27dfbc598e6ea66344a12211f679222a202f294b
Thammy Silva <thamirysmatos@yahoo.com.br>
2025-11-27 19:44:26 -0300

    Código Comentário do Usuário

---
+++ comentario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../styles/filme.css">
+
+    <title>Comentário 💬</title>
+</head>
+
+<body>
+    <form action="index.php" method="GET">
+        <h1> Comentário 💬 </h1>
+        <form action="" method="post">
+            <input type="text" name="comentario" id="comentario" placeholder="Comente sobre o filme">
+            <button type="submit">Enviar</button>
+        </form>
+</body>
+</html>
+
+<?php
+    $comentario =$_GET ['comentario'];
+    
+    echo "<p>$comentario</p>";
+?>
